962951b59d2136224f6e34ade278a0322cdbe356
ibragim955 <amerhanov_ibragim95@mail.ru>
2024-02-02 01:07:02 +0300

    калькулятор

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -99,6 +99,14 @@ public class Main {
         romanValues.put(5, "V");
         romanValues.put(9, "IX");
         romanValues.put(10, "X");
+        romanValues.put(40, "XL");
+        romanValues.put(50, "L");
+        romanValues.put(90, "XC");
+        romanValues.put(100, "C");
+        romanValues.put(400, "CD");
+        romanValues.put(500, "D");
+        romanValues.put(900, "CM");
+        romanValues.put(1000, "M");
 
         StringBuilder romanNumber = new StringBuilder();
         while (decimalNumber > 0) {
